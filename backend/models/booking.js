9890fe9cf109c532e525c56b01d3937057369423
AhanaGupta004 <ahanagupta.1213@gmail.com>
2026-09-12 21:29:53 +0530

    Create booking.js

---
+++ backend/models/booking.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true
+    },
+
+    bookingDate: {
+      type: Date,
+      default: Date.now
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "approved",
+        "confirmed",
+        "rejected",
+        "cancelled"
+      ],
+      default: "confirmed"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
